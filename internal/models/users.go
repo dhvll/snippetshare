e174f18b2da52144f8a25649474b143b71b1fcbe
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-12-14 16:58:38 +0530

    created user model

---
+++ internal/models/users.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+// user type
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+}
+
+// user Model
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	return nil
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	return 0, nil
+}
+
+func (m *UserModel) Exists(id int) (bool, error) {
+	return false, nil
+
+}
