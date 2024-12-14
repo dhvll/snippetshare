b44be0b49f7eeffbf531b374aac8c663639b1cee
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-12-15 02:51:04 +0530

    user authorization added

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -48,8 +48,9 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
-		CurrentYear: time.Now().Year(),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -80,4 +81,8 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
