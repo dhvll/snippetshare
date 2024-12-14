7ccb1dd1492d9cc801e70e0622f5bc417123e209
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-12-15 00:49:22 +0530

    user signup is completed

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -102,7 +102,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 type userSignupForm struct {
 	Name                string `form:"name"`
 	Email               string `form:"email"`
-	Password            string `form:password"`
+	Password            string `form:"password"`
 	validator.Validator `form:"-"`
 }
 
@@ -110,11 +110,49 @@ func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	data.Form = userSignupForm{}
 	app.render(w, http.StatusOK, "signup.tmpl", data)
-	fmt.Fprintln(w, "user signup")
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create new user ")
+
+	var form userSignupForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be minimum 8 chars")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
+		return
+	}
+
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.AddFieldError("email", "Email is already in use")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Your signup is sucessful. Please log in")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
